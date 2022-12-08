49519acd54bcad72e8313caa9f8cf8d5ae0a6ce9
ZaferKok <zaferkok111@gmail.com>
2022-12-08 02:54:25 +0100

    commit

--- src/githubLesson/githubFirst.java
+++ src/githubLesson/githubFirst.java
@@ -5,6 +5,7 @@ public class githubFirst {
     public static void main(String[] args){
 
         System.out.println("IntelliJ'den GitHub'a giden yol");
+        System.out.println("IntelliJ'den GitHub'a giden yol");
 
     }
 
